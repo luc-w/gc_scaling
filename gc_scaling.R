a0b17649571f636bad62d43f8bbab18bd9e1c754
Luc Watrin <luc.watrin@gmx.de>
2022-12-08 11:54:24 +0100

    download template, improved plot, minor improvements

--- gc_scaling.R
+++ gc_scaling.R
@@ -5,12 +5,21 @@
 library(shiny)
 library(shinydashboard)
 library(dashboardthemes)
-library(plotly)
 library(ggplot2)
+library(tidyr)
 library(markdown)
 
-# load validity data
-df_full <- read.csv2("gc_age_final_2020-08-27.csv")
+# static data sets
+short <- read.csv("5256_gc_3_12_best_models.csv", sep=";")
+df_full <- read.csv2("gc_age_final_2020-08-27.csv") %>% 
+           # compute total scores of short scales
+           mutate(gc1 = rowSums(select(., short[[1]]), na.rm = TRUE),
+                  gc2 = rowSums(select(., short[[2]]), na.rm = TRUE),
+                  gc3 = rowSums(select(., short[[3]]), na.rm = TRUE))
+sample_data <- data.frame(id = c(1,2,3,4,5),
+                          gc1 = c(5,8,9,12,11),
+                          gc2 = c(6,9,8,6,10), 
+                          gc3 = c(7,3,11,12,8))
 
 
 # define ui
@@ -21,6 +30,11 @@ ui <- dashboardPage(
   
   # Define inputs
   dashboardSidebar(
+    
+    # Input: Downloadable sample data for illustration ----
+    downloadButton(outputId = "download_sample_data", 
+                   label = "Download Sample Data"),
+    
     
     # Input: Select a file ----
     fileInput("file1", "Upload CSV File",
@@ -34,7 +48,7 @@ ui <- dashboardPage(
     
     # Input: Sex of norm sample ----
     selectInput("sex", "Sex",
-                c("All" = 3,
+                c("All" = "all",
                   "Male" = 2,
                   "Female" = 1)),
     
@@ -96,33 +110,49 @@ server <- function(input, output) {
                 
               read.csv(input$file1$datapath,
                                   header = TRUE,
-                                  sep = ";")
+                                  sep = ";") %>%
+              # remove completely empty rows (if scores from < 3 parallel tests are entered)
+              select(where(~ !(all(is.na(.)) | all(. == ""))))
     })
     
     ## Access and transform norm data set ----
     norm_data <- reactive({
       
-      df_full %>% 
+      if(input$sex == "all"){
+        
+        df_full %>% 
+        # age range
+        filter(age >= input$age_range[1] & age <= input$age_range[2]) %>% 
+        # highest education
+        filter(edu <= input$edu)
+        
+      } else {
+        
+        df_full %>% 
         # sex
         filter(sex == input$sex) %>% 
         # age range
         filter(age >= input$age_range[1] & age <= input$age_range[2]) %>% 
         # highest education
         filter(edu <= input$edu)
+
+      }
+      
+
       
     })
   
     ## Scale uploaded data based on norm data ----
     scaled_data <- reactive({
-      
-      scores() %>% 
-        mutate(zcg1 = (scores()$gc1 - mean(norm_data()$gc_total))/sd(norm_data()$gc_total),
-               zcg2 = (scores()$gc2 - mean(norm_data()$gc_total))/sd(norm_data()$gc_total),
-               zcg3 = (scores()$gc3 - mean(norm_data()$gc_total))/sd(norm_data()$gc_total))
+
+      # scale uploaded scores (if available) based on defined norm sample
+      scaled_data <- scores()
+      scaled_data <- if("gc1" %in% colnames(scores())) mutate(scaled_data, zcg1 = (scores()$gc1 - mean(norm_data()$gc1))/sd(norm_data()$gc1))
+      scaled_data <- if("gc2" %in% colnames(scores())) mutate(scaled_data, zcg2 = (scores()$gc2 - mean(norm_data()$gc2))/sd(norm_data()$gc2)) else scaled_data
+      scaled_data <- if("gc3" %in% colnames(scores())) mutate(scaled_data, zcg3 = (scores()$gc3 - mean(norm_data()$gc3))/sd(norm_data()$gc3)) else scaled_data
 
     })
-        
-        
+
     
     # Output: Score distribution ----
     output$plot1 <- renderPlot({
@@ -130,13 +160,23 @@ server <- function(input, output) {
                     if (is.null(data())) {
                       return(NULL)
                     }
-                     
-                    ggplot() + 
-                      # density plot of norm data
-                      geom_density(data = norm_data(), aes(x = gc_total*12), color = "blue") +
-                      # density plot of uploaded data
-                      geom_density(data = scores(), aes(x = gc1), color = "red") +
-                      scale_x_continuous(limits = c(0, 12), breaks = seq(0, 12, 1))
+                    
+                    # transform data sets of uploaded socres and norm sample to long format for plotting
+                    scores_long <- scores() %>% 
+                                   select(any_of(c("gc1", "gc2", "gc3"))) %>% 
+                                   pivot_longer(cols = everything(), names_to = "Scale", values_to = "Score") %>% 
+                                   mutate(Sample = "Uploaded")
+                    norm_data_long <- norm_data() %>% 
+                                      select(any_of(c("gc1", "gc2", "gc3"))) %>% 
+                                      pivot_longer(cols = everything(), names_to = "Scale", values_to = "Score") %>% 
+                                      mutate(Sample = "Norm Sample")
+                    
+                    rbind(scores_long, norm_data_long) %>% 
+                    ggplot(aes(x= Score, color = Sample)) + 
+                      geom_density() +
+                      scale_x_continuous(limits = c(0, 12), breaks = seq(0, 12, 1)) +
+                      facet_grid(vars(Scale)) +
+                      theme_bw()
                           
                   })
     
@@ -151,6 +191,12 @@ server <- function(input, output) {
                      
                     })
     
+    # Output: Imported data plus standardized scores ----
+    output$download_sample_data <- downloadHandler(
+                
+                                      write.csv2(sample_data, "sample_data.csv", row.names = FALSE)
+                                  
+                                      )
     
     # Output: Imported data plus standardized scores ----
     output$download_scaled_data <- downloadHandler(
