d8d9cddcf9db7c15fed7f1dc2183baade02df16d
Luc Watrin <luc.watrin@gmx.de>
2023-02-10 14:30:46 +0100

    added figure caption better variable names better comments removed old .md

--- gc_scaling.R
+++ gc_scaling.R
@@ -37,7 +37,11 @@ ui <- dashboardPage(
     # Input: Downloadable sample data for illustration ----
     downloadButton(outputId = "download_template", 
                    label = "Download Template",
-                   style = "color: #fff; background-color: #27ae60; border-color: #fff;padding: 5px 14px 5px 14px; margin: 5px 5px 20px 18px;"),
+                   style = "color: #fff; 
+                            background-color: #27ae60; 
+                            border-color: #fff;
+                            padding: 5px 14px 5px 14px; 
+                            margin: 5px 5px 20px 18px;"),
     
     # Input: Select a file ----
     fileInput("file1", "Upload CSV File",
@@ -67,7 +71,7 @@ ui <- dashboardPage(
                 value = c(18,70)),
     
     # Input: Sex of norm sample ----
-    selectInput("edu", "Highest education level",
+    selectInput("edu", "Highest Education Level",
                 c("University Degree" = 6,
                   "Vocational training" = 5,
                   "Academic track school" = 4, 
@@ -84,17 +88,20 @@ ui <- dashboardPage(
     # Input: Download data ----
     downloadButton(outputId = "download_scaled_data", 
                    label = "Download Scaled Data",
-                   style = "color: #fff; background-color: red; border-color: #fff;width:130;padding: 5px 5px 5px 5px; margin: 5px 5px 5px 16px; "),
+                   style = "color: #fff; 
+                            background-color: red; 
+                            border-color: #fff;
+                            width:130;
+                            padding: 5px 5px 5px 5px; 
+                            margin: 5px 5px 5px 16px; "),
     
     # horizontal line ----
     tags$hr(),
     
     # License
-    HTML(paste0("luc.watrin[at]uni-ulm.de", "<br>",
-                "CC-By Attribution 4.0 International", "<br>",
-                "Code available here: https://osf.io/xxx/"))
-    
-    
+    HTML("luc.watrin[at]uni-ulm.de <br>
+         CC-By Attribution 4.0 International <br>
+         Code available at https://github.com/luc-w/gc_scaling")
   ),
   
   
@@ -107,7 +114,7 @@ ui <- dashboardPage(
                   tabPanel("Score Distribution", 
                            plotOutput("plot1", width = 700, height = 700)),
                   tabPanel("Debug", 
-                           tableOutput("table1"))
+                           tableOutput("tab_debug"))
       ))
 )
 
@@ -134,19 +141,19 @@ server <- function(input, output) {
       if(input$sex == "all"){
         
         df_full %>% 
-        # age range
+        # filter cases that match user-defined range
         filter(age >= input$age_range[1] & age <= input$age_range[2]) %>% 
-        # highest education
+        # filter cases with user-defined highest education
         filter(edu <= input$edu)
         
       } else {
         
         df_full %>% 
-        # sex
+        # filter cases with user-defined sex
         filter(sex == input$sex) %>% 
-        # age range
+        # filter cases that match user-defined range
         filter(age >= input$age_range[1] & age <= input$age_range[2]) %>% 
-        # highest education
+        # filter cases with user-defined highest education
         filter(edu <= input$edu)
 
       }
@@ -158,7 +165,7 @@ server <- function(input, output) {
     ## Scale uploaded data based on norm data ----
     scaled_data <- reactive({
 
-      # scale uploaded scores (if available) based on defined norm sample
+      # scale uploaded scores (if available) based on (user-defined) norm sample
       scaled_data <- scores()
       scaled_data <- if("gc1" %in% colnames(scores())) mutate(scaled_data, zcg1 = (scores()$gc1 - mean(norm_data()$gc1))/sd(norm_data()$gc1))
       scaled_data <- if("gc2" %in% colnames(scores())) mutate(scaled_data, zcg2 = (scores()$gc2 - mean(norm_data()$gc2))/sd(norm_data()$gc2)) else scaled_data
@@ -170,7 +177,7 @@ server <- function(input, output) {
     # Output: Score distribution ----
     output$plot1 <- renderPlot({
               
-                    if (is.null(data())) {
+                    if (is.null(data())) { # do not return a plot without data
                       return(NULL)
                     }
                     
@@ -178,32 +185,43 @@ server <- function(input, output) {
                     scores_long <- scores() %>% 
                                    select(any_of(c("gc1", "gc2", "gc3"))) %>% 
                                    pivot_longer(cols = everything(), names_to = "Scale", values_to = "Score") %>% 
-                                   mutate(Sample = "Uploaded")
+                                   mutate(Sample = "Your Data")
                     norm_data_long <- norm_data() %>% 
                                       select(any_of(c("gc1", "gc2", "gc3"))) %>% 
                                       pivot_longer(cols = everything(), names_to = "Scale", values_to = "Score") %>% 
                                       mutate(Sample = "Norm Sample")
                     
+                    # figure caption
+                    fig_caption <- paste0(# norm sample size
+                                           "Norm sample size N = ", nrow(norm_data()), ", ",
+                                           # % female
+                                           round(prop.table(table(norm_data()$sex))["1"]*100, 2), "% female.")
+                    
+                    # Density plots: norm sample and user-uploaded data
                     rbind(scores_long, norm_data_long) %>% 
-                    ggplot(aes(x= Score, color = Sample)) + 
-                      geom_density() +
+                    ggplot(aes(x= Score, fill = Sample)) + 
+                      geom_density(alpha = .5) +
                       scale_x_continuous(limits = c(0, 12), breaks = seq(0, 12, 1)) +
+                      #scale_fill_manual(values=c("#CC6666", "#9999CC", "#66CC99")) + 
                       facet_grid(vars(Scale)) +
-                      theme_bw()
+                      labs(caption = fig_caption) + 
+                      theme_bw() +
+                      theme(plot.caption = element_text(size = 14, hjust = 0, margin = margin(t = 15)))
                           
                   })
     
-    # Output: Scaled data table
-    output$table1 <- renderTable({
-      
-                     if (is.null(scaled_data())) {
-                       return(NULL)
-                     }
-      
-                     scaled_data()
-                     
-                    })
     
+    # Output: Scaled data table
+    output$tab_debug <- renderTable({
+      
+                         if (is.null(scaled_data())) {
+                           return(NULL)
+                         }
+          
+                         scaled_data()
+                         
+                        })
+        
     # Output: Imported data plus standardized scores ----
     output$download_template <- downloadHandler(
                 
--- main.md
+++ main.md
@@ -1,5 +1,5 @@
 HOW TO
 
-1. Upload raw data. You can download a template to see what it should look like.
-2. Select the characteristics of your reference sample (Sex, Age, Education). The default is the sample reported in Watrin et al. (2022). 
-3. Download the scale (z-standardized) data.
+1. Upload your raw data. You can download a template to see what it should look like.
+2. Choose your preferences regarding the norm sample (sex, age, education). The default norm sample is the one reported in [Watrin et al. (2022)](https://psycnet.apa.org/record/2022-14618-001?doi=1). 
+3. Download the scaled (z-standardized) data.
